4429278d6eb012eb40cecc12078bdc3d847ef918
sherbieny <sherbieny@outlook.com>
2024-11-02 21:33:43 +0900

    Add sanctum routes for token Add sanctum middleware abilities

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/sanctum/token', [AuthController::class, 'createToken']);
+Route::post('/sanctum/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/books', [BookController::class, 'index']);
-    Route::post('/books', [BookController::class, 'store']);
-    Route::get('/books/{id}', [BookController::class, 'show']);
-    Route::put('/books/{id}', [BookController::class, 'update']);
-    Route::delete('/books/{id}', [BookController::class, 'destroy']);
+    Route::post('/books', [BookController::class, 'store'])->middleware('abilities:create-book');
+    Route::get('/books/{id}', [BookController::class, 'show'])->middleware('abilities:view-book');
+    Route::put('/books/{id}', [BookController::class, 'update'])->middleware('abilities:update-book');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->middleware('abilities:delete-book');
 });
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Book routes
     Route::get('/books', [BookController::class, 'index'])->name('books.index');
     Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
     Route::post('/books', [BookController::class, 'store'])->name('books.store');
